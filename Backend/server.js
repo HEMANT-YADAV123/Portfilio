8113afbeb594a75545a6d064e3d8ab89b52c6ba4
HEMANT-YADAV123 <hy700793@gmail.com>
2025-03-13 11:13:08 +0530

    updated the env file and server logic

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,33 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Server is running.');
+});
 
 app.post('/send-email', async (req, res) => {
     const { Username, phoneNumber, Email, Subject, Message } = req.body;
 
     try {
         const transporter = nodemailer.createTransport({
-            service: 'gmail', // e.g., Gmail
+            service: 'gmail',
             auth: {
-                user: 'hy700793@gmail.com',
-                pass: 'ybof wjak segj uynt'
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
             }
         });
 
         const mailOptions = {
             from: Email,
-            to: 'hy700793@gmail.com',
+            to: process.env.EMAIL_USER,
             subject: `New Contact Form Submission - ${Subject}`,
             text: `Name: ${Username}\nPhone: ${phoneNumber}\nEmail: ${Email}\n\nMessage:\n${Message}`
         };
@@ -28,7 +35,7 @@ app.post('/send-email', async (req, res) => {
         await transporter.sendMail(mailOptions);
         res.status(200).send("Email sent successfully!");
     } catch (error) {
-        console.error("Error details:", error);
+        console.error("Error details backend:", error);
         res.status(500).send("There was an error sending the email.");
     }
 });
